8740294d93359097ed46585a349adbccb4f5f289
tenghx <tenghx@users.noreply.github.com>
2020-08-02 08:43:12 +0000

    update tenghx/github-pages@3c7f66e7b33974576f45e7d6446e76f5932179c8

--- lightdark.js
+++ lightdark.js
@@ -14,7 +14,7 @@ function setTheme() {
         }
 
         if (date !== prev) {
-          fetch('https://' + window.location.hostname + '/astronomy')
+          fetch('https://api.ipgeolocation.io/astronomy?apiKey=0ccc869db7af49c5b332c106e7dd0d91')
             .then(res => res.json())
             .then(data => {
               sunrise = data.sunrise.split(':').map(Number);
